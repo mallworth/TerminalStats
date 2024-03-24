369854a9c956e84d709c455a9ebcc0ba0bfbd3f6
mallworth <maxallworthmiles@gmail.com>
2024-03-24 14:27:07 -0700

    main added from term.c, instructions added to README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Description: This program enhances the macOS terminal interface by displaying an ASCII Apple logo and system hardware statistics. 
 
+Instructions: Ensure the main file is located in your user directory or specify its path during compilation. Navigate to terminal settings -> shell -> startup, and enable 'Run Command'. For the command, type: `printf "\033c" && gcc main.c && ./a.out`.
+
 Author: Max Allworth-Miles
 
 License: This project is licensed under the MIT License.
---
+++ main.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <sys/utsname.h>
+#include <stdlib.h>
+#include <sys/sysctl.h>
+#include <time.h>
+#include <openGL/gl.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/ioctl.h>
+#include <sys/statvfs.h>
+
+
+char *execute_system_command(const char *command) {
+    FILE *pipe = popen(command, "r");
+
+    if (pipe == NULL) {
+        return NULL;
+    }
+
+    char *output = NULL;
+    char buffer[128];
+    size_t output_size = 0;
+
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+        size_t buffer_len = strlen(buffer);
+        output = realloc(output, output_size + buffer_len + 1);
+
+        if (output == NULL) {
+            fclose(pipe);
+            return NULL; 
+        }
+
+        strcpy(output + output_size, buffer);
+        output_size += buffer_len;
+    }
+
+    pclose(pipe);
+    return output;
+}
+
+
+int main() {
+	// Sys info
+    struct utsname unameData;
+    uname(&unameData);
+
+    // CPU info
+    char cpu[50];
+    size_t cpu_len = sizeof(cpu);
+    sysctlbyname("machdep.cpu.brand_string", &cpu, &cpu_len, NULL, 0);
+
+    // OS info
+    int mib[2];
+    size_t len;
+    char *kernel_version;
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_OSRELEASE;
+
+    sysctl(mib, 2, NULL, &len, NULL, 0);
+    kernel_version = (char *)malloc(len * sizeof(char));
+    sysctl(mib, 2, kernel_version, &len, NULL, 0);
+
+    // Shell info
+    FILE *f;
+    char version[50];
+    f = popen("bash --version | head -n 1", "r");
+    fgets(version, sizeof(version), f);
+    char *paren_pos = strchr(version, '(');
+    *paren_pos = '\0';
+    pclose(f);
+
+    // Print date in center
+	char *date = execute_system_command("date");
+
+    struct winsize w1;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w1);
+    int terminal_width1 = w1.ws_col;
+    int length1 = strlen(date);
+    int left_padding1 = (terminal_width1 - length1) / 2;
+
+    for (int i = 0; i < left_padding1; i++) {
+        printf(" ");
+    }
+
+    printf("%s", date);
+
+    // Print host and username in center
+    char hostname[200];
+    char *username = getlogin();
+    gethostname(hostname, sizeof(hostname));
+
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int terminal_width = w.ws_col;
+    int length = strlen(username) + strlen(hostname) + 1;
+    int left_padding = (terminal_width - length) / 2;
+
+    for (int i = 0; i < left_padding; i++) {
+		printf(" ");
+    }
+
+    printf("%s@%s\n", username, hostname);
+
+    // System memory
+    long pages = sysconf(_SC_PHYS_PAGES);
+    long page_size = sysconf(_SC_PAGE_SIZE);
+    long total_memory = pages * page_size;
+
+    // Filesystem information
+    struct statvfs buf;
+    statvfs("/", &buf);
+    unsigned long long total_space = buf.f_frsize * buf.f_blocks;
+
+	// APPLE
+  	printf("\033[1;32m");
+    printf("\n\t\t    'c.");
+	printf("\033[0m");
+
+	printf("\033[0;37m");
+	printf("     			OS: ");
+	printf("\033[0m");
+	printf("%s %s\n", unameData.sysname, kernel_version);
+	free(kernel_version);
+	printf("\033[1;32m");
+
+    printf("\t\t,xNMM.");
+
+	printf("\033[0;37m");
+	printf("     			Architecture: ");
+	printf("\033[0m");
+	printf("%s\n", unameData.machine);
+	printf("\033[1;32m");
+
+    printf("\t      .OMMMMo");
+
+	printf("\033[0;37m");
+	printf("     			CPU: ");
+	printf("\033[0m");
+	printf("%s\n", cpu);
+	printf("\033[1;32m");
+
+    printf("\t      OMMM0");
+
+	printf("\033[0;37m");
+	printf("     			Shell: ");
+	printf("\033[0m");
+	printf("%s\n", version);
+	printf("\033[1;32m");
+
+    printf("      .;loddo:' loolloddol;.");
+
+	printf("\033[0;37m");
+	printf("     		Memory: ");
+	printf("\033[0m");
+	printf("%ld ", total_memory);
+	printf("\033[0;37m");
+	printf("bytes\n");
+	printf("\033[1;32m");
+
+    printf("    cKMMMMMMMMMMNWMMMMMMMMMM0:");
+
+	printf("\033[0;37m");
+	printf("     		Disk Space: ");
+	printf("\033[0m");
+	printf("%llu ", total_space);
+	printf("\033[0;37m");
+	printf("bytes\n");
+	printf("\033[1;32m");
+
+	printf("\033[1;33m");
+	printf("  .KMMMMMMMMMMMMMMMMMMMMMMMWd.\n");
+	printf("  XKMMMMMMMMMMMMMMMMMMMMMMX.\n");
+	printf("\033[1;31m");
+	printf(" ;MMMMMMMMMMMMMMMMMMMMMMMM:\n");
+	printf(" :MMMMMMMMMMMMMMMMMMMMMMMM:\n");
+	printf(" .MMMMMMMMMMMMMMMMMMMMMMMMX.\n");
+	printf("  kMMMMMMMMMMMMMMMMMMMMMMMWd.\n");
+	printf("\033[0m");
+	printf("\033[1;35m");
+	printf("  .XMMMMMMMMMMMMMMMMMMMMMMMMMk\n");
+	printf("   .XMMMMMMMMMMMMMMMMMMMMMMK.\n");
+	printf("\033[0m");
+	printf("\033[1;34m");
+	printf("     kMMMMMMMMMMMMMMMMMMMMMd\n");
+	printf("      ;KMMMMMMMWXXWMMMMMMMk.\n");
+	printf("        .cooc,.    .,coo:.\n\n");
+	printf("\033[0m");
+}
